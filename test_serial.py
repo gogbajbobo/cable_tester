23b922c22659e02e29361f14b14c6bccf145b20e
gogbajbobo <grigoriev.maxim@gmail.com>
2025-03-21 12:56:42 +0300

    Update test_serial.py

--- test_serial.py
+++ test_serial.py
@@ -1,3 +1,4 @@
+import serial
 import serial.tools.list_ports
 
 print("Start")
@@ -9,6 +10,12 @@ print(ports)
 
 for port, desc, hwid in sorted(ports):
     print("{}: {} [{}]".format(port, desc, hwid))
+
+# ser = serial.Serial("COM11", 9600)
+# if ser.is_open:
+#     print(f"{ser} is open")
+#     while True:
+#         print(ser.readline())
 
 
 print("Finish")
